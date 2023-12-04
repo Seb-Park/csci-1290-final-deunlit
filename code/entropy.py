93a3195ea7c85774abbd40457eb50e477b32445d
LyatteLiu <yutang_liu@brown.edu>
2023-12-04 17:09:12 -0500

    Revise energy function calculation

--- code/entropy.py
+++ code/entropy.py
@@ -1,47 +1,93 @@
 import numpy as np
 from scipy.optimize import least_squares
 from utils import gaussian_kernel
-
 import matplotlib.pyplot as plt
 
-def calculate_pixel_differences(image):
-    # Assuming image is grayscale
-    image = image.astype(np.float32)
+EPSILON = 1e-6 # to avoid log 0
 
-    # Initialize arrays to store differences
-    # log_illumination_diffs = np.zeros_like(image)
+def mean_neighbor_log_intensity_differences(image):
+    '''
+    For each pixel in the image,
+    calculates the mean of log intensity differences between the pixel and its neighbors.
+    Should be returning a matrix of shape (height, width). (Same as image)
+    Assuming image is grayscale
+    '''
+    h, w = image.shape[0], image.shape[1]
     mean_log_intensity_diffs = np.zeros_like(image)
-    log_image = np.log(image + 1)  # avoid log(0)
+    log_image = np.log(image + EPSILON)  # avoid log(0)
 
-    for i in range(1, image.shape[0]-1):
-        for j in range(1, image.shape[1]-1):
-            neighborhood = log_image[i-1:i+2, j-1:j+2]
-            diff = np.abs(neighborhood - log_image[i, j])
-            # log_illumination_diffs[i, j] = np.sum(diff) - diff[1, 1]  # Exclude the center pixel
-            mean_log_intensity_diffs[i, j] = np.abs(log_image[i, j] - np.mean(neighborhood))
+    for r in range(h):
+        for c in range(w):
+            # neighborhood with boundary considerations
+            top = max(r-1, 0)
+            bottom = min(r+1, h-1)
+            left = max(c-1, 0)
+            right = min(c+1, w-1)
+            neighborhood = [
+                log_image[top, c], 
+                log_image[bottom, c], 
+                log_image[r, left],
+                log_image[r, right]
+            ]
+            mean_log_intensity_diffs[r, c] = np.abs(
+                log_image[r, c] - np.mean(neighborhood))
 
     return mean_log_intensity_diffs
 
 
-def energy_function(l, image, lambda_reg):
+def energy_function(curr_L, image, lambda_reg, phi_l, phi_p, omega_t):
     """
-    Define the energy function to be minimized.
-    :param l: Current illumination estimate.
-    :param image: Input image.
-    :param lambda_reg: Regularization parameter.
+    Define the energy function to be minimized according to equation 16.
+    :param curr_L: Current illumination estimate, (h, w)
+    :param image: Input image, (h, w)
+    :param lambda_reg: Regularization parameter, (1,)
+    :param phi_l, phi_p, omega_t: smoothing matrices to calculate weights u and v
     :return: Energy value for the current illumination estimate.
     """
-    u = calculate_weights_u(image)
-    v = calculate_weights_v(image)
-    eta_bar = calculate_pixel_differences(image)
+    h, w = image.shape[0], image.shape[1]
+    # u and v should be sparse matrix of (h*w, h*w)
+    u = calculate_weights_u(image, phi_l, phi_p)
+    v = calculate_weights_v(image, omega_t)
+    eta_bar = mean_neighbor_log_intensity_differences(image)
 
-    # First term: smoothness of illumination field
-    smoothness_term = np.sum(u * l**2)
+    L_star = np.zeros((h, w))
 
-    # Second term: texture term
-    texture_term = lambda_reg * np.sum(v * (l - eta_bar)**2)
+    # using r and c for row and col to avoid confusing with pixel i and neighbor j
+    for r in range(h):
+        for c in range(w):
+            # Handle boundaries
+            top = max(r-1, 0)
+            bottom = min(r+1, h-1)
+            left = max(c-1, 0)
+            right = min(c+1, w-1)
 
-    return smoothness_term + texture_term
+            log_L_i = np.log(curr_L[r, c] + EPSILON) # log illumination of current pixel
+            l_ijs = [
+                np.abs(log_L_i - np.log(curr_L[top, c] + EPSILON)),
+                np.abs(log_L_i - np.log(curr_L[bottom, c] + EPSILON)),
+                np.abs(log_L_i - np.log(curr_L[r, left] + EPSILON)),
+                np.abs(log_L_i - np.log(curr_L[r, right] + EPSILON))
+            ]
+            u_ijs = [
+                u[r*c, top*c],
+                u[r*c, bottom*c],
+                u[r*c, r*left],
+                u[r*c, r*right],
+            ]
+            v_ijs = [
+                v[r*c, top*c],
+                v[r*c, bottom*c],
+                v[r*c, r*left],
+                v[r*c, r*right],
+            ]
+
+            smoothness_term = u_ijs * l_ijs**2
+            texture_term = v_ijs * (l_ijs - eta_bar[r, c])**2
+
+            L_star[r, c] = smoothness_term + lambda_reg * texture_term
+
+    return L_star
+
 
 def minimize_energy(image, initial_l, lambda_reg):
     """
@@ -51,34 +97,37 @@ def minimize_energy(image, initial_l, lambda_reg):
     :param lambda_reg: Regularization parameter.
     :return: Optimal illumination estimate.
     """
-    result = least_squares(energy_function, initial_l, args=(image, lambda_reg))
-    return result.x
-
-
-def calculate_weights_u(image):
     pass
 
-def calculate_weights_v(image):
+
+def calculate_weights_u(image, phi_l, phi_p):
     pass
 
-rgb2gray_weightr = 0.2125 
+
+def calculate_weights_v(image, omega_t):
+    pass
+
+
+rgb2gray_weightr = 0.2125
 rgb2gray_weightg = 0.7154
 rgb2gray_weightb = 0.0721
 
+
 def find_luminance_chrominance(image):
-    luminance = np.average(image, axis=2, weights=[rgb2gray_weightr, \
-                                                   rgb2gray_weightg, \
+    luminance = np.average(image, axis=2, weights=[rgb2gray_weightr,
+                                                   rgb2gray_weightg,
                                                    rgb2gray_weightb])
     chrom_r, chrom_g, chrom_b = image[:, :, 0] / luminance, \
-                                image[:, :, 1] / luminance, \
-                                image[:, :, 2] / luminance
-    chrominance = np.dstack([np.clip(chrom_r, 0, 255), \
-                             np.clip(chrom_g, 0, 255), \
+        image[:, :, 1] / luminance, \
+        image[:, :, 2] / luminance
+    chrominance = np.dstack([np.clip(chrom_r, 0, 255),
+                             np.clip(chrom_g, 0, 255),
                              np.clip(chrom_b, 0, 255)])
     plt.imshow(luminance, cmap="gray")
     # # plt.imshow(chrominance)
     plt.show()
     return np.clip(luminance, 0, 255), np.clip(chrominance, 0, 255)
+
 
 def calculate_illumination(image):
     # SEPARATE OUT LUMINANCE AND CHROMINANCE
