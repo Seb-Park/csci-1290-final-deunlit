058e5e0f3e29025137e126015ad814a98ab0cc04
Sebastian Park <SebPark03@gmail.com>
2023-12-11 05:00:40 -0500

    Try to implement multi-scale--fail.

--- code/entropy.py
+++ code/entropy.py
@@ -65,7 +65,7 @@ EPSILON = 1e-6  # to avoid log 0
 #     return L_star
 
 
-def minimize_energy(image, mask, initial_l, phi_l, phi_p, omega_t, omega_p, lambda_reg=1.0, num_iter=10, maxiter=10000, tol=1e-5):
+def minimize_energy(image, mask, initial_l, phi_l, phi_p, omega_t, omega_p, lambda_reg=1.0, num_iter=10, maxiter=10000, tol=1e-5, img_name='1171'):
     """
     Minimize the energy function using iterative optimization.
     :return: Optimal illumination estimate (IN LOG DOMAIN!!!!!!!!!!)
@@ -113,11 +113,73 @@ def minimize_energy(image, mask, initial_l, phi_l, phi_p, omega_t, omega_p, lamb
             curr_l.reshape((image.shape[0], image.shape[1])).astype(np.int64)
         curr_image = np.multiply(np.exp(curr_l.reshape(
             (image.shape[0], image.shape[1]))), np.exp(optimal_r)).astype(np.uint8)
-        cv2.imwrite(f'../results/test_1171_new15_{iteration}.jpg', curr_image)
+        cv2.imwrite(f'../results/test_{img_name}_new15_{iteration}.jpg', curr_image)
         # curr_image = apply_new_illumination(curr_image, curr_l.reshape((h,w)))
 
     return curr_l
 
+def minimize_energy_pyramid(image, mask, initial_l, phi_l, phi_p, omega_t, omega_p, lambda_reg=1.0, num_iter=5, maxiter=10000, tol=1e-5, img_name='1171'):
+    """
+    Minimize the energy function using iterative optimization.
+    :return: Optimal illumination estimate (IN LOG DOMAIN!!!!!!!!!!)
+    """
+    # image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    h, w = image.shape[0], image.shape[1]
+    num_pixels = h * w
+
+    curr_l = initial_l
+    pyramid = create_image_pyramid(image, num_iter, rev=True)
+    curr_image = pyramid[0]
+    mask_pyramid = create_image_pyramid(mask, num_iter, rev=True)
+    # mask = mask.reshape((num_pixels, 1))
+    for iteration in range(num_iter):
+        print(f'------------Iteration: {iteration}-------------')
+        # curr_image = pyramid[iteration]
+        h, w = curr_image.shape[0], curr_image.shape[1]
+        num_pixels = h * w
+        mask = mask_pyramid[iteration].reshape((num_pixels, 1))
+        u = calculate_weights_u(curr_image, phi_l, phi_p)
+        v = calculate_weights_v(curr_image, omega_t, omega_p)
+        eta_bar = mean_neighbor_log_intensity_differences(
+            curr_image).reshape((num_pixels, 1))
+        uv_sum = u + (lambda_reg * v)
+        A = sparse.diags(np.array(uv_sum.sum(axis=1)).flatten())
+        # is_valid_A, message = is_symmetric_and_positive_definite_sparse(A)
+        # if not is_valid_A:
+        #     print("A doesn't seem right")
+        #     print(message)
+        #     # print(check_matrix_properties_sparse(A))
+        # else:
+        #     print("A seems legit")
+        b = uv_sum * curr_l + lambda_reg * \
+            (sparse.diags(np.array(v.sum(axis=1)).flatten()) - v) * eta_bar
+
+        print(f'eta_bar.shape={eta_bar.shape}')
+        print(f'A.shape={A.shape}')
+        print(f'b.shape={b.shape}')
+        b[mask == 0] = 0
+        curr_l, exit_code = sparse.linalg.cg(A, b, x0=curr_l, maxiter=maxiter, tol=tol)
+        curr_l = curr_l.reshape((num_pixels, 1))
+        print(f'curr_l.shape={curr_l.shape}')
+        if exit_code == 0:
+            print('CG SOLVER SUCCESS')
+        elif exit_code > 0:
+            print('CG SOLVER DID NOT CONVERGE')
+        else:
+            print('CG SOLVER ILLEGAL INPUT')
+
+        optimal_r = np.log(image + EPSILON).astype(np.int64) - \
+            curr_l.reshape((image.shape[0], image.shape[1])).astype(np.int64)
+        optimal_r = scale_up(optimal_r)
+        print(pyramid[iteration+1].shape)
+        print(optimal_r.shape)
+        curr_image = np.multiply(np.exp(curr_l.reshape(
+            (pyramid[iteration + 1].shape[0], 
+             pyramid[iteration + 1].shape[1]))), np.exp(optimal_r)).astype(np.uint8)
+        cv2.imwrite(f'../results/test_{img_name}_new15_{iteration}.jpg', curr_image)
+        # curr_image = apply_new_illumination(curr_image, curr_l.reshape((h,w)))
+
+    return curr_l
 
 def mean_neighbor_log_intensity_differences(image):
     '''
@@ -176,6 +238,11 @@ def calculate_weights_u(image, phi_l, phi_p):
         ###       that not get weighted?
         neighbors = [(curr_row - 1, curr_col), (curr_row + 1, curr_col),
                      (curr_row, curr_col - 1), (curr_row, curr_col + 1)]
+        # neighbors = [(curr_row, curr_col),
+        #              (curr_row - 1, curr_col), (curr_row + 1, curr_col),
+        #              (curr_row, curr_col - 1), (curr_row, curr_col + 1),
+        #              (curr_row + 1, curr_col + 1), (curr_row - 1, curr_col + 1),
+        #              (curr_row - 1, curr_col - 1), (curr_row + 1, curr_col - 1)]
         for dir in neighbors:
             if dir[0] < 0 or dir[0] >= image.shape[0] or dir[1] < 0 or dir[1] >= image.shape[1]:
                 continue
@@ -330,6 +397,9 @@ def create_image_pyramid(img, iterations, rev=False):
         img = scale_image(img, 2)
         res.append(img)
     return res[::-1] if rev else res
+
+def scale_up(img):
+    return img.repeat(2, axis=0).repeat(2, axis=1)
 
 # def minimize_energy_with_pyramid(image, initial_l, phi_l, phi_p, omega_t, omega_p, lambda_reg=1.0, num_iter=5):
 #     """
